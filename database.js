8aa61abff0beee48191fc9ba4a410ce180a123b4
UseMe24 <usemeanywhere100@gmail.com>
2026-07-15 02:41:52 +0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,25 @@
+
+/*{
+    "siteName": "",
+    "category": "", //"Movie & Series", "Other", App
+    "siteFeatures": [''],
+    "url": "",
+    "tags": [""]
+}*/
+const movieDatabase = [
+    {
+        "siteName": "HDHUB4U",
+        "category": "Movie & Series",
+        "siteFeatures": [
+            'One Ad Per Page but not annoying',
+            'Hollywood',
+            'Bollywood',
+            'South Indian',
+            'Hindi Dubbed',
+            'Korean',
+        ],
+        'image': "https://images.unsplash.com/photo-1783763625078-e015b4b42a92?q=80&w=726&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "url": "https://hdhub4u.tv/",
+        "tags": ["Movie", "Series"]
+    }
+];
---
+++ main.js
@@ -0,0 +1,60 @@
+const moviesContainer = document.getElementById('movies-container');
+const seriesContainer = document.getElementById('series-container');
+const appsContainer = document.getElementById('apps-container');
+
+
+function initDirectory() {
+    movieDatabase.forEach(site => {
+        let featuresList = '<ul>';
+        site.siteFeatures.forEach(feature => { featuresList += `<li>${feature}</li>`; });
+        featuresList += '</ul>';
+
+        let tagsList = '<div class="tags-container">';
+        site.tags.forEach(tag => { tagsList += `<span class="tag">#${tag}</span>`; });
+        tagsList += '</div>';
+
+        const cardHtml = `
+                    <div class="card">
+                        <div>
+                            <div class="siteDetails" id="hello">
+                                <h3>${site.siteName}</h3>
+                            </div>
+                                    <img src="${site.image}" alt="sitePreview">
+                            ${featuresList}
+                        </div>
+                        <div>
+                            ${tagsList}
+                            <a href="${site.url}" target="_blank" class="btn">Visite Now</a>
+                        </div>
+                    </div>
+                `;
+
+
+        if (site.category === 'Movie & Series') moviesContainer.innerHTML += cardHtml;
+        else if (site.category === 'Other') seriesContainer.innerHTML += cardHtml;
+        else if (site.category === 'Apps') appsContainer.innerHTML += cardHtml;
+    });
+
+
+
+    document.getElementById('movies-section').classList.add('active');
+    document.getElementById('menu-movies').classList.add('active');
+}
+
+function switchTab(event, targetCategory) {
+    event.preventDefault();
+    document.getElementById('movies-section').classList.remove('active');
+    document.getElementById('series-section').classList.remove('active');
+    document.getElementById('apps-section').classList.remove('active');
+
+    document.getElementById('menu-movies').classList.remove('active');
+    document.getElementById('menu-series').classList.remove('active');
+    document.getElementById('menu-apps').classList.remove('active');
+
+
+    document.getElementById(`${targetCategory}-section`).classList.add('active');
+    document.getElementById(`menu-${targetCategory}`).classList.add('active');
+}
+
+window.onload = initDirectory;
+
