0811288527a6bc9f9643eaf9055a1886b4aa5d9e
UseMe24 <usemeanywhere100@gmail.com>
2026-07-15 16:34:22 +0300

    details set

--- database.js
+++ database.js
@@ -31,11 +31,12 @@ const movieDatabase = [
         ],
         "url": "https://hdhub4u.tv/",
         "tags": ["Movie", "Series"],
-        "html": `<deatils>
-      <img src="https://images.unsplash.com/photo-1783763625078-e015b4b42a92?q=80&w=726&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-  <summury>
+        "html": `<details>
+        <summury>
   Preview Site
   </summury>
+      <img src="https://images.unsplash.com/photo-1783763625078-e015b4b42a92?q=80&w=726&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+  
 </details>`
     },
     {
