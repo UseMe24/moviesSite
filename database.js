b7477ac5ddae30b39c83f978604e66ab14ec6fe2
UseMe24 <usemeanywhere100@gmail.com>
2026-07-15 16:33:14 +0300

    new

--- database.js
+++ database.js
@@ -31,7 +31,12 @@ const movieDatabase = [
         ],
         "url": "https://hdhub4u.tv/",
         "tags": ["Movie", "Series"],
-        "html": `<deatils>Preview<summury><img src="https://images.unsplash.com/photo-1783763625078-e015b4b42a92?q=80&w=726&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></summury></details>`
+        "html": `<deatils>
+      <img src="https://images.unsplash.com/photo-1783763625078-e015b4b42a92?q=80&w=726&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+  <summury>
+  Preview Site
+  </summury>
+</details>`
     },
     {
         "siteName": "Movie Box",
